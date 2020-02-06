96590a7d4792dee75c0cb4347db2e7ade0966a20
[Dan Farrow] <dan@zellement.com>
2020-02-06 21:49:03 +0000

    Remove from tailwind config, amend nav css

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,6 @@ module.exports = {
       },
     }
   },
-  plugins: [
-    require('tailwindcss-transition')({
-      standard: 'all .3s ease',
-      transitions: {
-        'slow': 'all 0.7s ease',
-      }  
-    })
-  ],
   corePlugins: {
     container: false
   }
